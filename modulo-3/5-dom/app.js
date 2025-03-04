679f4b0649618944445179f23cca33edec037b1c
Jesus Sanchez <work.prof.jash@gmail.com>
2025-03-03 20:37:43 -0600

    Caja de Comentarios (Simple Comment Box) practica

--- modulo-3/5-dom/app.js
+++ modulo-3/5-dom/app.js
@@ -1,17 +1,36 @@
-// Cada libro debe ser un objeto con las siguientes propiedades: titulo: (string) el título del libro, autor: (string) el autor del libro, anio: (number) el año de publicación, estado: (string) el estado del libro, que puede ser 'disponible' o 'prestado'.
-// También debe tener un método describirLibro: (method) método para imprimir la información básica del libro. Algo como 'Libro titulado [titulo], escrito por [autor] en el año [anio], el estado es: [estado].'
-// Opcional: agregar una propiedad que contenga la lista de capítulos del libro y métodos que permitan agregar y eliminar capítulos del libro.
+const inputComment = document.getElementById("inputComentario")
+const btnComment = document.getElementsByTagName("button")[0]
+const boxComments = document.getElementsByClassName("comentarios")[0]
+const comments = [];
+let i = 0;
 
-const book = {
-    titulo: (String),
+let comentar = (event) => {
+    event.preventDefault(); // Previene que se actualice el sitio al dar click en comentar
 
-    autor: (string),
-  
-    anio: (number),
-  
-    estado: (string),
-  
-  };
+    const fecha = new Date();
+    const hoy = fecha.toLocaleString();
 
-  
+    const comment = {
+        id: i++, // Incrementa el id único para cada comentario
+        date: hoy,
+        comment: inputComment.value,
+        user: "pepe"
+    };
+    comments.push(comment);
+    printComment(comments); // Llama a la función para mostrar los comentarios
+    inputComment.value = ""; // Limpia el campo de texto después de comentar
+};
 
+let printComment = (array) => {
+  boxComments.innerHTML = ""; // Limpia la lista de comentarios antes de imprimir los nuevos
+
+  array.forEach(item => {
+      const { id, date, comment, user } = item;
+
+      const li = document.createElement("li");
+      li.innerHTML = `${user}<br><b>${comment}</b> ⌚ ${date}`;
+      
+      boxComments.appendChild(li); // Agrega el comentario a la lista
+  });
+};
+btnComment.addEventListener("click", comentar); 
